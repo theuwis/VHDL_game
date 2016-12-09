dc083b273507b16499dfe393fede3fe95da6c051
Tibo <tibo.theuwis@gmail.com>
2016-12-09 18:24:11 +0100

    updated variable names

--- sim_1/new/DRAW_BLOCK_TB.vhd
+++ sim_1/new/DRAW_BLOCK_TB.vhd
@@ -11,10 +11,10 @@ architecture Behavioral of DRAW_BLOCK_TB is
 				RST : in STD_LOGIC;
 				X_POS_CURRENT : in STD_LOGIC_VECTOR(8 downto 0);
 				Y_POS_CURRENT : in STD_LOGIC_VECTOR(8 downto 0);
-				X_DESIRED_1 : in INTEGER;
-				X_DESIRED_2 : in INTEGER;
-				Y_DESIRED_1 : in INTEGER;
-				Y_DESIRED_2 : in INTEGER;
+				X_1 : in INTEGER;
+				X_2 : in INTEGER;
+				Y_1 : in INTEGER;
+				Y_2 : in INTEGER;
 				DRAW : out BOOLEAN);
 	end component;
 	
@@ -22,15 +22,15 @@ architecture Behavioral of DRAW_BLOCK_TB is
 	signal RST : STD_LOGIC;
 	signal X_POS : STD_LOGIC_VECTOR(8 downto 0);
 	signal Y_POS : STD_LOGIC_VECTOR(8 downto 0);
-	signal X_DES1 : INTEGER;
-	signal X_DES2 : INTEGER;
-	signal Y_DES1 : INTEGER;
-	signal Y_DES2 : INTEGER;
+	signal X_1 : INTEGER;
+	signal X_2 : INTEGER;
+	signal Y_1 : INTEGER;
+	signal Y_2 : INTEGER;
 	constant clock_period : time := 8ns;
 begin
 
 block_test: DRAW_BLOCK port map(CLK => CLK, RST => RST, X_POS_CURRENT => X_POS, Y_POS_CURRENT => Y_POS,
-								X_DESIRED_1 => X_DES1, X_DESIRED_2 => X_DES2, Y_DESIRED_1 => Y_DES1, Y_DESIRED_2 => Y_DES2);
+								X_1 => X_1, X_2 => X_2, Y_1 => Y_1, Y_2 => Y_2);
 
 clk_process: process
 	begin
@@ -42,10 +42,10 @@ end process;
 
 inf: process
 	begin
-		X_DES1 <= 100;
-		X_DES2 <= 200;
-		Y_DES1 <= 100;
-		Y_DES2 <= 200;
+		X_1 <= 100;
+		X_2 <= 200;
+		Y_1 <= 100;
+		Y_2 <= 200;
 		X_POS <= "001101001"; -- 105
 		Y_POS <= "001101001"; -- 105
 		-- DRAW should be TRUE
@@ -60,7 +60,7 @@ inf: process
 		-- DRAW should be FALSE
 		wait for clock_period*2;
 		
-		Y_DES2 <= 300;
+		Y_2 <= 300;
 		-- DRAW should be TRUE
 		wait;
 end process;
