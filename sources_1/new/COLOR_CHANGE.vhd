d57d32efd78ce943c47e219e2131263ccdeb4093
Tibo <tibo.theuwis@gmail.com>
2016-12-28 19:57:17 +0100

    seperate files for touch control of game

---
+++ sources_1/new/COLOR_CHANGE.vhd
@@ -0,0 +1,89 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity COLOR_CHANGE is
+	port(	CLK : in STD_LOGIC;
+			RST : in STD_LOGIC;
+			X_TOUCH : in STD_LOGIC_VECTOR (7 downto 0);
+			Y_TOUCH : in STD_LOGIC_VECTOR (7 downto 0);
+			
+			BLOCK_COL : out STD_LOGIC_VECTOR (23 downto 0);
+			LEDS : OUT STD_LOGIC_VECTOR(3 downto 0));
+end COLOR_CHANGE;
+
+architecture Behavioral of COLOR_CHANGE is
+
+begin
+process(Y_TOUCH)
+	variable COUNT_RED : INTEGER RANGE 0 TO 100000;
+	variable COUNT_PINK : INTEGER RANGE 0 TO 100000;
+	variable COUNT_GREEN : INTEGER RANGE 0 TO 100000;
+	variable COUNT_CYAN: INTEGER RANGE 0 TO 100000;
+	
+	begin
+	if (CLK'event and CLK = '1') then
+--		if Y_TOUCH < "00100100" then
+		if X_TOUCH > "00100110" and X_TOUCH < "00101110" and Y_TOUCH < "00101010" then
+			COUNT_RED  := 0;
+			COUNT_PINK := COUNT_PINK + 1;
+			COUNT_GREEN:= 0;
+			COUNT_CYAN := 0;
+		
+			LEDS(3) <= '1';
+		
+			if COUNT_PINK = 100000 then
+				BLOCK_COL(23 downto 16) <= "11111111";
+				BLOCK_COL(15 downto 8) <=  "00000000";
+				BLOCK_COL(7 downto 0) <=   "11111111";
+			end if;
+		elsif X_TOUCH > "01000101" and X_TOUCH < "01001111" and Y_TOUCH < "00100111" then
+			COUNT_RED  := 0;
+			COUNT_PINK := 0;
+			COUNT_GREEN:= COUNT_GREEN + 1;
+			COUNT_CYAN := 0;
+		
+			LEDS(2) <= '1';
+			
+			if COUNT_GREEN = 100000 then
+				BLOCK_COL(23 downto 16) <= "00000000";
+				BLOCK_COL(15 downto 8) <=  "11111111";
+				BLOCK_COL(7 downto 0) <=   "00000000";
+			end if;
+		elsif X_TOUCH > "11000011" and X_TOUCH < "11001100" and Y_TOUCH < "00100100" then
+			COUNT_RED  := 0;
+			COUNT_PINK := 0;
+			COUNT_GREEN:= 0;
+			COUNT_CYAN := COUNT_CYAN + 1;
+		
+			LEDS(1) <= '1';
+			
+			if COUNT_CYAN = 100000 then
+				BLOCK_COL(23 downto 16) <= "00000000";
+				BLOCK_COL(15 downto 8) <=  "11111111";
+				BLOCK_COL(7 downto 0) <=   "11111111";
+			end if;
+			
+		elsif X_TOUCH > "11010000" and X_TOUCH < "11011000" and Y_TOUCH < "00100100" then
+			COUNT_RED  := COUNT_RED + 1;
+			COUNT_PINK := 0;
+			COUNT_GREEN:= 0;
+			COUNT_CYAN := 0;
+			
+			LEDS(0) <= '1';
+		
+			if COUNT_RED = 100000 then
+				BLOCK_COL(23 downto 16) <= "11111111";
+				BLOCK_COL(15 downto 8) <=  "00000000";
+				BLOCK_COL(7 downto 0) <=   "00000000";
+			end if;
+		else
+			LEDS <= "0000";
+			COUNT_RED  := 0;
+			COUNT_PINK := 0;
+			COUNT_GREEN:= 0;
+			COUNT_CYAN := 0;
+		end if;	
+	end if;
+end process;
+
+end Behavioral;
---
+++ sources_1/new/POSITION_CHANGE.vhd
@@ -0,0 +1,112 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity POSITION_CHANGE is
+	port(	CLK : in STD_LOGIC;
+			RST : in STD_LOGIC;
+			
+			X_TOUCH : in STD_LOGIC_VECTOR(7 downto 0);
+			Y_TOUCH : in STD_LOGIC_VECTOR(7 downto 0);
+			X_POS : in STD_LOGIC_VECTOR(8 downto 0);
+			Y_POS : in STD_LOGIC_VECTOR(8 downto 0);
+			DRAW_MOVING_BLOCK : out BOOLEAN);
+end POSITION_CHANGE;
+
+architecture Behavioral of POSITION_CHANGE is
+	signal ROW1 : BOOLEAN;
+	signal ROW2 : BOOLEAN;
+	signal ROW3 : BOOLEAN;
+	
+begin
+
+process(CLK)
+	begin
+	if (CLK'event and CLK = '1') then
+		if X_POS > "110100100" and Y_POS < "011001011" and (ROW1 = true or ROW2 = true or ROW3 = true) then
+			if ROW1 = true then
+				if Y_POS > "000000100" and Y_POS < "001000011" then
+					DRAW_MOVING_BLOCK <= true;
+				else
+					DRAW_MOVING_BLOCK <= false;
+				end if;
+					
+			elsif ROW2 = true then
+				if Y_POS > "001001010" and Y_POS < "01000011" then
+					DRAW_MOVING_BLOCK <= true;
+				else
+					DRAW_MOVING_BLOCK <= false;
+				end if;
+				
+			elsif ROW3 = true then
+				if Y_POS > "010001101" and Y_POS < "011001011" then
+					DRAW_MOVING_BLOCK <= true;
+				else
+					DRAW_MOVING_BLOCK <= false;
+				end if;
+			else
+				DRAW_MOVING_BLOCK <= false;		
+			end if;
+		else
+			DRAW_MOVING_BLOCK <= false;	
+		end if;
+	end if;
+end process;
+
+process(Y_TOUCH)
+	variable COUNT_ROW1 : INTEGER RANGE 0 TO 100000;
+	variable COUNT_ROW2 : INTEGER RANGE 0 TO 100000;
+	variable COUNT_ROW3 : INTEGER RANGE 0 TO 100000;
+	
+	begin
+	if (CLK'event and CLK = '1') then
+		if X_TOUCH > "11100000" then
+			if Y_TOUCH > "11010110" and Y_TOUCH < "11100111" then
+				COUNT_ROW1  := COUNT_ROW1 + 1;
+				COUNT_ROW2  := 0;
+				COUNT_ROW3  := 0;
+							
+				if COUNT_ROW1 >= 100000 then
+					ROW1 <= true;
+					ROW2 <= false;
+					ROW3 <= false;
+				end if;
+				
+			elsif Y_TOUCH > "10110001" and Y_TOUCH < "10111111" then
+				COUNT_ROW1  := 0;
+				COUNT_ROW2  := COUNT_ROW2 + 1;
+				COUNT_ROW3  := 0;
+		
+				if COUNT_ROW2 >= 100000 then
+					ROW1 <= false;
+					ROW2 <= true;
+					ROW3 <= false;
+				end if;
+				
+			elsif Y_TOUCH > "00101100" and Y_TOUCH < "00111010" then
+				COUNT_ROW1  := 0;
+				COUNT_ROW2  := 0;
+				COUNT_ROW3  := COUNT_ROW3 + 1;
+							
+				if COUNT_ROW3 >= 100000 then
+					ROW1 <= false;
+					ROW2 <= false;
+					ROW3 <= true;
+				end if;	
+
+			else
+				COUNT_ROW1  := 0;
+				COUNT_ROW2  := 0;
+				COUNT_ROW3  := 0;
+			end if;
+		else
+			COUNT_ROW1  := 0;
+			COUNT_ROW2  := 0;
+			COUNT_ROW3  := 0;	
+		end if;
+	end if;
+end process;
+
+
+end Behavioral;
